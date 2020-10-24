678d0a29de8e05309c1bdac659ed67cbb4fe6410
Cam corder <corder2425@yahoo.com>
2020-10-24 13:22:15 -0500

    CodeQuiz

---
+++ questions.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        title: "What is the object called that lets you work with both dates and time-related data?:",
+        choices: ["Dates", "Time-warp", "Clock","Time Field:"],
+        answer: "Dates"
+    },
+    {
+        title: "What elements are used to test for TRUE or False values stored in variables?:",
+        choices: ["Conditional statements", "RegExp or Regular Expressions", "Trigger readers", "Comparison and logical operators."],
+        answer: "Comparison and logical operators."
+    },
+    {
+        title: "What is considered to be the most popular programming language in the world?:",
+        choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+        answer: "Both 1 and 3"
+    },
+    {
+        title: "What is the default behavior called that is used to move declarations to the top of the current scope?:",
+        choices: ["Sorting", "Arranging", "Hoisting", "Jumping"],
+        answer: "Jumping"
+    },
+    {
+        title: "What attribute is used in html to decorate content?:",
+        choices: ["style", "class", "src", "css"],
+        answer: "style"
+    }
+]
+
+
